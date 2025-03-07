bf89abe12fd7d515cac9f4836f3d85bd1a88f7a6
Kushagra Saxena <kushagrasaxena061@gmail.com>
2025-03-07 20:09:09 +0530

    backend complete

---
+++ src/app/(protected)/dashboard/page.tsx
@@ -0,0 +1,24 @@
+// scenario 1 -> repeated user
+// redirect to respective dashboard 
+
+// scenario 2 -> new user
+// redirect -> sign in
+
+import { onBoardUser, onCurrentUser } from '@/actions/user';
+import { redirect } from 'next/navigation';
+import React from 'react'
+
+type Props = {}
+
+const Page = async (props: Props) => {
+  console.log('dash k andar-->')
+  const user = await onBoardUser();
+  console.log('dasmh-->',user.data?.firstname, user.data?.lastname)
+  if (user.status === 200 || user.status === 201) {
+    return redirect(`dashboard/${user.data?.firstname}${user.data?.lastname}`)
+  }
+
+  return redirect('/sign-in')
+}
+
+export default Page
